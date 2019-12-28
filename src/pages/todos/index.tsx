76dd9dd52d2a3f1c0b9adc93ee2ef3d7bdf6734e
coacer <coacer728h23@icloud.com>
2019-12-28 20:57:10 +0900

    todosをフェッチまでできた

--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { RootState } from "store/reducers";
+import { fetchTodos } from "store/actions/todos";
+import { Todo } from "interfaces";
+
+const todosSelector = (state: RootState) => state.todos;
 
 const TodosIndex: React.FC = () => {
+  const todos = useSelector(todosSelector);
+  const dispatch = useDispatch();
+
+  useEffect((): void => {
+    if (todos.length > 0) return;
+    dispatch(fetchTodos());
+  });
+
+  const todosElements: React.ReactElement[] = todos.map((todo: Todo) => (
+    <li key={todo.id}>{todo.id} / {todo.title} / {todo.completed ? "true" : "false"}</li>
+  ));
+
   return (
-    <h1>Todos</h1>
+    <ul>
+      {todosElements}
+    </ul>
   );
 };
 
