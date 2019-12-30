f729e45df74554fd2902d6f6604156f8c2ec288c
coacer <coacer728h23@icloud.com>
2019-12-30 17:56:21 +0900

    Improved UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from "react-redux";
-import store from "store";
+import Container from '@material-ui/core/Container';
 
+import store from "store";
 import TodosIndex from "pages/todos";
 import TodosNew from "pages/todos/new";
 import Header from "components/layouts/Header";
@@ -12,10 +13,12 @@ import Footer from "components/layouts/Footer";
 const App: React.FC = () => (
   <Router>
     <Header />
-    <Provider store={store}>
-      <Route exact path="/" component={TodosIndex} />
-      <Route exact path="/todos/new" component={TodosNew} />
-    </Provider>
+    <Container maxWidth="md">
+      <Provider store={store}>
+        <Route exact path="/" component={TodosIndex} />
+        <Route exact path="/todos/new" component={TodosNew} />
+      </Provider>
+    </Container>
     <Footer />
   </Router>
 );
--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Checkbox, Radio, Button } from '@material-ui/core';
 
 import { RootState } from "interfaces";
 import { fetchTodos, delTodo, checkTodo } from "store/actions/todos";
@@ -32,35 +33,47 @@ const TodosIndex: React.FC = () => {
   }, [dispatch]);
 
   const todosElements: React.ReactElement[] = todos.map((todo: Todo) => (
-    <li key={todo.id}>
-      <input
-        type="checkbox"
+    <li
+      key={todo.id}
+      style={{ listStyle: "none" }}
+    >
+      <Checkbox
         checked={todo.completed}
         onChange={handleChangeCheckbox(todo.id)}
+        value="primary"
       />
-      {todo.id} / {todo.title} / {todo.completed ? "true" : "false"}
-      <button onClick={handleClickDelete(todo.id)}>Delete</button>
+      {todo.id} / {todo.title}
+      {/* <button onClick={handleClickDelete(todo.id)}>Delete</button> */}
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={handleClickDelete(todo.id)}
+          style={{ marginLeft: "20px" }}
+        >
+          Delete
+      </Button>
     </li>
   ));
 
   return (
     <>
-      <input
-        type="radio"
+      <Radio
         checked={!isFilterTodos}
         onChange={() => setIsFilterTodos(false)}
-      /> All
+      />
+      All
       <br />
-      <input
-        type="radio"
+      <Radio
         checked={isFilterTodos}
         onChange={() => setIsFilterTodos(true)}
-      /> Not Completed
+      />
+      Not Completed
       <ul>
         {todosElements}
       </ul>
     </>
   );
+
 };
 
 export default TodosIndex;
