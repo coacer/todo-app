d4d194ce0f5982d85fef9852585909b2c9745da9
coacer <coacer728h23@icloud.com>
2020-01-01 21:12:54 +0900

    TodoItem test ok

--- src/components/todos/TodoItem/__test__/index.spec.tsx
+++ src/components/todos/TodoItem/__test__/index.spec.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import TodoItem, { TodoItemProps } from "../";
+import { Checkbox, Button } from '@material-ui/core';
+import { delTodo, checkTodo } from "store/actions/todos";
 
+// dispatchメソッドのモック作成
 const mockDispatch = jest.fn()
 
+// Providerでstoreを渡してないからredux hooksのモックを作成
+// モックを作らない場合hooks呼び出しでエラーになる
+// モックを作成する以外の対処法として、単純にstoreを作ってProviderでラップする方法もあるが
+// Unitテストにならないのでモックを使用
 jest.mock('react-redux', () => ({
     useSelector: jest.fn(fn => fn()),
     useDispatch: () => mockDispatch,
@@ -13,21 +20,54 @@ jest.mock('react-redux', () => ({
 describe('<TodoItem />', () => {
 
   afterEach(() => {
-    jest.restoreAllMocks()
+    // それぞれのテスト終了後にモックを初期化
+    jest.resetAllMocks()
   });
 
-  it('display a todo information', () => {
-    const props: TodoItemProps = {
-      todo: {
-        id: 1,
-        title: "Jestとenzymeを勉強する",
-        completed: false
-      }
-    };
+  // ダミーデータ
+  const props: TodoItemProps = {
+    todo: {
+      id: 1,
+      title: "Jestとenzymeを勉強する",
+      completed: false
+    }
+  };
 
+  it('display informations of todo', () => {
+
+    // shallowでコンポーネントをレンダリング
+    // shallowは子コンポーネントをレンダリングしない
+    // 逆にmountはレンダリングする
     const wrapper = shallow(<TodoItem {...props} />);
 
     expect(wrapper.find(".todo-id").text()).toBe("1");
     expect(wrapper.find(".todo-title").text()).toBe("Jestとenzymeを勉強する");
   });
+
+  it('display components', () => {
+    const wrapper = shallow(<TodoItem {...props} />);
+    // shallowでレンダリングしてるためfindで子コンポーネントを直接指定できる
+    // mountの場合は再帰的に全てレンダリングされるため, 実際に描画される要素をする('button'とか)
+    expect(wrapper.find(Button).length).toBe(1);
+    expect(wrapper.find(Checkbox).length).toBe(1);
+  });
+
+  it('should dispatch be called when Button is clicked', () => {
+    const wrapper = shallow(<TodoItem {...props} />);
+    // sinulateでイベント発火
+    wrapper.find(Button).simulate('click');
+    // モックは何回呼び出されたか、何を引数に渡されたかなどを知っている
+    expect(mockDispatch.mock.calls.length).toBe(1);
+    expect(mockDispatch).toHaveBeenCalledWith(delTodo(props.todo.id));
+  });
+
+  it('should dispatch be called when Checkbox is changed', () => {
+    const wrapper = shallow(<TodoItem {...props} />);
+    // sinulateでイベント発火
+    wrapper.find(Checkbox).simulate('change');
+    // モックは何回呼び出されたか、何を引数に渡されたかなどを知っている
+    expect(mockDispatch.mock.calls.length).toBe(1);
+    expect(mockDispatch).toHaveBeenCalledWith(checkTodo(props.todo.id));
+  });
+
 });
